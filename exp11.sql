4a2f82d0c3a38d47e50ec2b2488721aac5ee470c
Neha Maria Joji <145547103+Nehamariaj@users.noreply.github.com>
2024-09-30 12:22:24 +0530

    DCL commands

--- exp11.sql
+++ exp11.sql
@@ -1,7 +1,7 @@
-grant select on bank to c22csa65;
-revoke select on bank from c22csa65;
+grant select on bank to c22csa65;--to grant select permission to another user
+revoke select on bank from c22csa65;--to take back the grant permission given to another user
 select * from c22csa65.book_issue;
-update c22csa65.book_issue set date_of_join='31/12/2005' where issue_id=4001;
+update c22csa65.book_issue set date_of_join='31/12/2005' where issue_id=4001;--updating another user table values
 
 commit;
 grant all on bank to c22csa65;
